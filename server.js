f90395c283fecfa2d57c728b2ace98a2d8ccf968
Nayan Prakash <107613181+nayanjee@users.noreply.github.com>
2022-08-01 21:59:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,8 +31,10 @@ app.use(express.urlencoded({ extended: true }));
 const db = require("./app/models");
 const Portal = db.portal;
 
+
+// for local change localhost to 65.1.82.24
 db.mongoose
-  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+  .connect("mongodb://larenonAdmin:EpMAYqj2nIHDFDSADAS32@localhost:27017/larenon-master?authSource=admin&retryWrites=false", {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
