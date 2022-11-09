327ea442ae3f3ef8a0908f0dd8e8c07ce39485ac
unknown <Nayan.Prakash@LARENON.NET>
2022-11-09 12:49:10 +0530

    modified motor get api

--- app/controllers/ins_motor.controller.js
+++ app/controllers/ins_motor.controller.js
@@ -74,29 +74,7 @@ exports.getData = function(req, res) {
     if (error) return res.status(400).send({status:400, message: 'problemFindingRecord'});
     if (!result) return res.status(200).send({status:400, message: 'noRecord'});
 
-    const finalResult = [];
-    result.forEach(element => {
-      const due = moment(element.dueDate).format('DD-MM-YYYY');
-      const pushData = {
-        _id:  element._id,
-        make: element.make,
-        model: element.model,
-        manufactureAt: element.manufactureAt,
-        regNo: element.regNo,
-        chachisNo: element.chachisNo,
-        idv: element.idv,
-        proposer: element.proposer,
-        insurer: element.insurer,
-        policyNo: element.policyNo,
-        premium: element.premium,
-        dueDate: due,
-        isActive: element.isActive,
-        isDeleted: element.isDeleted
-      }
-      finalResult.push(pushData);
-    });
-
-    res.status(200).send({status:200, message:'Success', data:finalResult});
+    res.status(200).send({status:200, message:'Success', data:result});
   }).sort({name : 1});
 };
 
